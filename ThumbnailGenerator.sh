940bb8f1a09328dedd98ac17abc32bb95a10e841
Alexandre de Lemeny-Makedone <13944652+nitatemic@users.noreply.github.com>
2023-12-09 13:55:04 +0100

    Update ThumbnailGenerator.sh
    Affiche les informations du fichier

--- ThumbnailGenerator.sh
+++ ThumbnailGenerator.sh
@@ -37,14 +37,40 @@ for video_file in "$videos_folder"/*; do
                 segment_end=$(bc -l <<< "$duration * ($i + 1) / 16")
 
                 # Générer des miniatures pour chaque segment
-                ffmpeg -ss "$segment_start" -i "$video_file" -vframes 1 -vf "scale=1920:1080" "$output_folder/${video_name}_thumbnail_$i.jpg"
+                ffmpeg -ss "$segment_start" -i "$video_file" -hide_banner -loglevel error -nostats -vframes 1 -vf "scale=1920:-1" "$output_folder/${video_name}_thumbnail_$i.jpg"
             done
 
             # Créer une mosaïque à partir des miniatures
             montage -tile 4x4 -geometry 1920x1080+0+0 "$output_folder/${video_name}_thumbnail_"*.jpg "$output_folder/${video_name}_mosaic.jpg"
         
+            # Création de l'image noire
+            convert -size 7680x400 xc:black "$output_folder/image_noire.jpg"
+        
+            # Récupérer les informations de la vidéo
+            video_title=$(basename "$video_file")
+            video_info=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration -of default=noprint_wrappers=1:nokey=1 "$video_file")
+            resolution=$(echo "$video_info" | awk -F 'x' '{printf "%dx%d", $1, $2}')
+            duration=$(ffprobe -i "$video_file" -show_entries format=duration -v quiet -of csv="p=0")
+            duration_timecode=$(ffmpeg -i "$video_file" 2>&1 | awk '/Duration:/ {print $2}' | tr -d ,)
+            duration_timecode=$(ffmpeg -i "$video_file" 2>&1 | awk '/Duration:/ {print $2}' | tr -d , | awk -F ':' '{printf "%02d:%02d:%02d", $1, $2, $3}')
+            file_size=$(du -h "$video_file" | cut -f1)
+
+            # Concaténer les informations dans la variable texte
+            texte="Filename: $video_title\nSize: $file_size\nResolution: $resolution\nLenght: $duration_timecode"
+
+            convert "$output_folder/image_noire.jpg" -fill white -pointsize 80 -gravity West -annotate +50+0 "$texte" "$output_folder/image_avec_texte.jpg"
+        
+            image1="$output_folder/image_avec_texte.jpg"
+            image2="$output_folder/${video_name}_mosaic.jpg"
+            
+            # Récupérer les dimensions des images
+            dimensions_image1=$(identify -format "%wx%h" "$image1")
+            dimensions_image2=$(identify -format "%wx%h" "$image2")
+            montage "$image1" "$image2" -geometry +0+0 -background none -mode concatenate -tile 1x2 "$output_folder/final.jpg"
+        
             # Supprimer les miniatures individuelles après la création de la mosaïque
-            rm "$output_folder/${video_name}_thumbnail_"*.jpg
+            #rm "$output_folder/${video_name}_thumbnail_"*.jpg
+            #rm "$output_folder/${video_name}_mosaic"*.jpg
         else
             echo "Le fichier '$video_file' n'est pas une vidéo et sera ignoré."
         fi
