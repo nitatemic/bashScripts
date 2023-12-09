090d33d47eb7806d83965e708d957db40f9998ed
Alexandre de Lemeny-Makedone <13944652+nitatemic@users.noreply.github.com>
2023-12-09 13:59:10 +0100

    Update ThumbnailGenerator.sh
    English and delete temp files

--- ThumbnailGenerator.sh
+++ ThumbnailGenerator.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-# Vérifier si un dossier a été passé en paramètre
+# Check if a directory has been passed as a parameter
 if [ $# -eq 0 ]; then
-    echo "Veuillez spécifier le chemin du dossier contenant les vidéos."
+    echo "Please specify the path to the folder containing the videos."
     exit 1
 fi
 
 videos_folder="$1"
 
-# Vérifier si le dossier existe
+# Check if the folder exists
 if [ ! -d "$videos_folder" ]; then
-    echo "Le dossier spécifié n'existe pas."
+    echo "The specified folder does not exist."
     exit 1
 fi
 
-# Parcourir toutes les vidéos du dossier
+# Browse through all the videos in the folder
 for video_file in "$videos_folder"/*; do
     if [ -f "$video_file" ]; then
-        # Vérifier si c'est un fichier vidéo
+        # Check if it's a video file
         file_type=$(file -b --mime-type "$video_file")
         if [[ $file_type == video/* ]]; then
-            # Extraire le nom du fichier sans extension
+            # Extract the file name without extension
             video_name=$(basename "$video_file")
-            video_name="${video_name%.*}"  # Retirer l'extension
+            video_name="${video_name%.*}"  # Remove extension
 
-            # Créer un dossier pour chaque vidéo s'il n'existe pas déjà
+            # Create a folder for each video if it doesn't exist already
             output_folder="$videos_folder/${video_name}"
             mkdir -p "$output_folder"
 
-            # Obtenir la durée totale de la vidéo
+            # Get the total duration of the video
             duration=$(ffprobe -i "$video_file" -show_entries format=duration -v quiet -of csv="p=0")
 
-            # Diviser la vidéo en 16 segments égaux et générer des miniatures
+            # Divide the video into 16 equal segments and generate thumbnails
             for ((i = 0; i < 16; i++)); do
                 segment_start=$(bc -l <<< "$duration * $i / 16")
                 segment_end=$(bc -l <<< "$duration * ($i + 1) / 16")
 
-                # Générer des miniatures pour chaque segment
+                # Generate thumbnails for each segment
                 ffmpeg -ss "$segment_start" -i "$video_file" -hide_banner -loglevel error -nostats -vframes 1 -vf "scale=1920:-1" "$output_folder/${video_name}_thumbnail_$i.jpg"
             done
 
-            # Créer une mosaïque à partir des miniatures
+            # Create a mosaic from the thumbnails
             montage -tile 4x4 -geometry 1920x1080+0+0 "$output_folder/${video_name}_thumbnail_"*.jpg "$output_folder/${video_name}_mosaic.jpg"
         
-            # Création de l'image noire
+            # Create the black image
             convert -size 7680x400 xc:black "$output_folder/image_noire.jpg"
         
-            # Récupérer les informations de la vidéo
+            # Retrieve video information
             video_title=$(basename "$video_file")
             video_info=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration -of default=noprint_wrappers=1:nokey=1 "$video_file")
             resolution=$(echo "$video_info" | awk -F 'x' '{printf "%dx%d", $1, $2}')
@@ -55,24 +55,26 @@ for video_file in "$videos_folder"/*; do
             duration_timecode=$(ffmpeg -i "$video_file" 2>&1 | awk '/Duration:/ {print $2}' | tr -d , | awk -F ':' '{printf "%02d:%02d:%02d", $1, $2, $3}')
             file_size=$(du -h "$video_file" | cut -f1)
 
-            # Concaténer les informations dans la variable texte
-            texte="Filename: $video_title\nSize: $file_size\nResolution: $resolution\nLenght: $duration_timecode"
+            # Concatenate information into the text variable
+            text="Filename: $video_title\nSize: $file_size\nResolution: $resolution\nLength: $duration_timecode"
 
-            convert "$output_folder/image_noire.jpg" -fill white -pointsize 80 -gravity West -annotate +50+0 "$texte" "$output_folder/image_avec_texte.jpg"
+            convert "$output_folder/image_noire.jpg" -fill white -pointsize 80 -gravity West -annotate +50+0 "$text" "$output_folder/image_with_text.jpg"
         
-            image1="$output_folder/image_avec_texte.jpg"
+            image1="$output_folder/image_with_text.jpg"
             image2="$output_folder/${video_name}_mosaic.jpg"
             
-            # Récupérer les dimensions des images
+            # Retrieve image dimensions
             dimensions_image1=$(identify -format "%wx%h" "$image1")
             dimensions_image2=$(identify -format "%wx%h" "$image2")
-            montage "$image1" "$image2" -geometry +0+0 -background none -mode concatenate -tile 1x2 "$output_folder/final.jpg"
-        
-            # Supprimer les miniatures individuelles après la création de la mosaïque
-            #rm "$output_folder/${video_name}_thumbnail_"*.jpg
-            #rm "$output_folder/${video_name}_mosaic"*.jpg
+            montage "$image1" "$image2" -geometry +0+0 -background none -mode concatenate -tile 1x2 "$output_folder/${video_name}_thumbnails.jpg"
+
+            # Remove individual thumbnails after creating the mosaic
+            rm "$output_folder/${video_name}_thumbnail_"*.jpg
+            rm "$output_folder/${video_name}_mosaic"*.jpg
+            rm "$output_folder/image_noire.jpg"
+            rm "$output_folder/image_with_text.jpg"
         else
-            echo "Le fichier '$video_file' n'est pas une vidéo et sera ignoré."
+            echo "The file '$video_file' is not a video and will be ignored."
         fi
     fi
 done
